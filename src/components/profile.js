4919bfb17989402b72040c56fe90999c66e5ea47
MarSchlaefer <mschlaefer00@gmail.com>
2019-04-05 12:17:20 -0400

    Added edit profile button

--- src/components/profile.js
+++ src/components/profile.js
@@ -6,8 +6,10 @@ class Profile extends Component {
     return (
       <div className="user-profile">
         <h1>{this.props.currUser.username}</h1>
+        <p>{this.props.currUser.bio}</p>
         <p>Followers: {this.props.currUser.followers.length}</p>
         <p>Following: {this.props.currUser.followees.length}</p>
+        <button onClick={HANDLE CLICK}>Edit Profile</button>
       </div>
     )
   }
@@ -15,6 +17,7 @@ class Profile extends Component {
   findBookObj = () => {
     return this.props.allBooks.find(book => book.id === this.props.currBookId)
   }
+
 } //end of class
 
 export default Profile;
